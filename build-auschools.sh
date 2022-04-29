c87bf0d85028c2299ad568e4f986182637cb8640
sannorozco <santiagorozco@gmail.com>
2022-04-29 17:52:52 -0500

    AU School fonts done with kerning
    —Re-spaced problematic glyphs in NSW-ACT, QLD, SA, TAS, and VIC-WA-NT
    —Added kerning groups & basic kerning on all masters
    —Renamed masters to Bold and match OS/2 700
    —Updated designspaces & build sh

--- build-auschools.sh
+++ build-auschools.sh
@@ -2,7 +2,7 @@
 
 source tools/auschool-env/bin/activate
 
-fontmake -m NSW-ACT-School-Fonts/NSW-ACT-Foundation.designspace -o variable --output-path fonts/NSW-ACT-Foundation[wght].ttf
+fontmake -m NSW-ACT-School-Fonts/NSW-ACT-Foundation.designspace -o variable --output-path fonts/NSW-ACT-Foundation-Beginners[wght].ttf
 
 fontmake -m QLD-School-Fonts/QLD-Beginners.designspace -o variable --output-path fonts/QLD-Beginners[wght].ttf
 
